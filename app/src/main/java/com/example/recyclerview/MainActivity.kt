832455ba72b363e0c403010226ab62f0ca3260e3
Hamjoshua <biryukov-10@list.ru>
2024-10-14 11:07:24 +0700

    Первое подзадание выполнено

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.recyclerview
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.ColorSpace
+import android.graphics.ColorSpace.Model
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rView: RecyclerView = findViewById(R.id.rView)
+        rView.layoutManager = LinearLayoutManager(this)
+        rView.adapter = Adapter(this, updateList())
     }
+
+    private fun updateList() : ArrayList<ColorData> {
+        val list = arrayListOf<ColorData>()
+
+        list.add(
+            ColorData(
+            "RED", Color.RED
+            )
+        )
+
+        list.add(
+            ColorData(
+                "BLUE", Color.BLUE
+            )
+        )
+
+        list.add(
+            ColorData(
+                "GREEN", Color.GREEN
+            )
+        )
+
+        return list;
+    }
+}
+
+data class ColorData(val colorName: String, val colorHex: Int){
+
 }
