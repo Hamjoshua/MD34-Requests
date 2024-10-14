f3e5b96cc50089a23549155f439fd9619fc239ce
Hamjoshua <biryukov-10@list.ru>
2024-10-14 12:48:42 +0700

    Подзадание 2 выполнено
    Получение ответа с помощью OkHttp

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Website.URL
 import android.util.Log
 import android.widget.Button
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.IOException
 import java.io.BufferedInputStream
 import java.io.InputStream
 import java.net.HttpURLConnection
@@ -17,9 +22,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.btnHTTP)
-        button.setOnClickListener{
+        val buttonHttp = findViewById<Button>(R.id.btnHTTP)
+        buttonHttp.setOnClickListener{
             logConnection()
+        }
+
+        val buttonOkHttp = findViewById<Button>(R.id.btnOkHTTP)
+        buttonOkHttp.setOnClickListener{
+            logOkConnection()
         }
     }
 
@@ -29,6 +39,12 @@ class MainActivity : AppCompatActivity() {
                 "64&tags=cat&format=json&nojsoncallback=1"
 
         HttpGetRequest().execute(stringUrl)
+    }
+
+    private fun logOkConnection(){
+        val stringUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
+                "&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
+        OkGetRequest().execute(stringUrl)
     }
 }
 
@@ -45,5 +61,28 @@ class HttpGetRequest : AsyncTask<String, Void, String>(){
     override fun onPostExecute(result: String) {
         Log.println(Log.DEBUG, "Flickr cats", result)
     }
+}
 
+class OkGetRequest : AsyncTask<String, Void, String>() {
+    private val client = OkHttpClient()
+
+    @Throws(IOException::class)
+    override fun doInBackground(vararg params: String): String? {
+        val url = params[0]
+        var result: String?
+
+        val request = Request.Builder().url(url).build()
+        try {
+            client.newCall(request).execute()
+                .use { response -> result = response.body?.string() }
+        } catch (e: IOException) {
+            result = e.message
+        }
+
+        return result;
+    }
+
+    override fun onPostExecute(result: String?) {
+        Log.println(Log.INFO, "Flickr cats", result.toString());
+    }
 }
